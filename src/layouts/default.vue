3088f99e264f81008b0ae99ecc9cc41455dda07e
steven <ste85213@gmail.com>
2024-10-04 17:57:07 +0800

    first commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-navigation-drawer>
+      <v-list>
+        <v-card
+        
+        >
+          <v-card-text class="text-center">
+            <router-link to="/" >
+              <v-img src="/src/assets/永信旅遊LOGO.png" width="150"></v-img>
+            </router-link>
+          </v-card-text>
+        </v-card>
+        
+        <v-list-item
+        v-for="navItem in navItems"
+        :key= "navItem.to"
+        :to = "navItem.to"
+        :prepend-icon="navItem.icon"
+        >
+        {{ navItem.text }}
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <v-main>
+      <router-view />
+    </v-main>
+
+  </v-app>
+</template>
+
+<script setup>
+  const navItems = [
+    { to: 'minutes', text: '會議記錄', icon: 'mdi-note-edit-outline'},
+    { to: 'admin/user', text: '使用者', icon: 'mdi-account'},
+    { to: 'login', text: '登入', icon: 'mdi-account'},
+  ]
+</script>
